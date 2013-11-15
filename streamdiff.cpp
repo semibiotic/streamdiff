97e4a4780a606a828fd671ff3417745f93641ff9
Ilya A. Kovalenko <semibiotic@gmail.com>
2013-11-15 14:54:55 +0400

    IMPR: Resonable run time, etc.
    [+] store position and re-seek on search continue
    [+] option to suppress context lines

--- streamdiff.cpp
+++ streamdiff.cpp
@@ -1,8 +1,12 @@
+#define _LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 int64_t    start_line[2]   = {0, 0};
 int64_t    cur_line[2]     = {0, 0};
@@ -16,10 +20,11 @@ char linebuf1[1024];
 char linebuf2[1024];
 
 bool isCycled = false;
+bool diffOnly = false;
 
 void usage()
 {
-    fprintf(stderr, "Usage: showdiff [-sN[,M]] [-c] FILE1 [FILE2]\n");
+    fprintf(stderr, "Usage: streamdiff [-sN[,M]] [-c] FILE1 [FILE2]\n");
 }
 
 int main(int argc, char ** argv)
@@ -30,8 +35,10 @@ int main(int argc, char ** argv)
     int meet[2];
     int n;
 
+    off64_t stored_offsets[2]  = {0, 0};
+    int64_t stored_linenums[2] = {0, 0};
 
-    while ((opt = getopt(argc, argv, "s:c")) != -1) {
+    while ((opt = getopt(argc, argv, "s:cd")) != -1) {
         switch (opt) {
         case 's':
             start_line[0] = strtol(optarg, &ptr, 10);
@@ -42,6 +49,9 @@ int main(int argc, char ** argv)
             break;
         case 'c':
             isCycled = true;
+            break;
+        case 'd':
+            diffOnly = true;
             break;
         default: /* '?' */
             usage();
@@ -64,170 +74,215 @@ int main(int argc, char ** argv)
         exit(EXIT_FAILURE);
     }
 
-while (true) {
+    while (true) {
 
-    FILE   * file[2] = {NULL, NULL};
+        int     fds[2];
 
-    for (i=0; i < 2; i++) {
-        if (strcmp(filename[i], "-") != 0)
-            file[i] = fopen(filename[i], "r");
-        else
-            file[i] = stdin;
+        FILE   * file[2] = {NULL, NULL};
 
-        if (!file[i]) {
-            perror("fopen");
+    // Open files, seek to stored lines
+        for (i=0; i < 2; i++) {
+            if (strcmp(filename[i], "-") != 0) {
+                fds[i] = open(filename[i], O_RDONLY);
+                if (fds[i] >= 0) {
+                    if (stored_offsets[i] == lseek64(fds[i], stored_offsets[i], SEEK_SET)) {
+                        lseek64(fds[i], stored_offsets[i], SEEK_SET);
+                        cur_line[i] = stored_linenums[i];
+                    }
+                    else {
+                        lseek64(fds[i], 0, SEEK_SET);
+                        cur_line[i] = 0;
+                    }
+
+                    file[i] = fdopen(fds[i], "r");
+                }
+            }
+            else {
+                file[i] = stdin;
+            }
+
+            if (!file[i]) {
+                perror("fopen");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (file[0] == file[1]) {
+            usage();
             exit(EXIT_FAILURE);
         }
-    }
 
-    if (file[0] == file[1]) {
-        usage();
-        exit(EXIT_FAILURE);
-    }
-
-    cur_line[0] = 0;
-    cur_line[1] = 0;
-
-    for (i=0; i<5; i++) {
-        if (prevContext[i] != NULL) {
-            free(prevContext[i]);
-            prevContext[i] = NULL;
+        for (i=0; i<5; i++) {
+            if (prevContext[i] != NULL) {
+                free(prevContext[i]);
+                prevContext[i] = NULL;
+            }
+            if (postContext[0][i] != NULL) {
+                free(postContext[0][i]);
+                postContext[0][i] = NULL;
+            }
+            if (postContext[1][i] != NULL) {
+                free(postContext[1][i]);
+                postContext[1][i] = NULL;
+            }
         }
-        if (postContext[0][i] != NULL) {
-            free(postContext[0][i]);
-            postContext[0][i] = NULL;
-        }
-        if (postContext[1][i] != NULL) {
-            free(postContext[1][i]);
-            postContext[1][i] = NULL;
-        }
-    }
 
-    if (start_line[0] != start_line[1]) {
-        int         fileInd   = start_line[0] > start_line[1] ? 0 : 1;
-        int64_t     skipLines = llabs(start_line[0] - start_line[1]);
-
-        for (i=0; i < skipLines; i++) {
-            if (!fgets(linebuf1, sizeof(linebuf1), file[fileInd])) {
+    // Skip until start positions
+        while (start_line[0] > cur_line[0]) {
+            if (!fgets(linebuf1, sizeof(linebuf1), file[0])) {
                 perror("skip");
                 exit(EXIT_FAILURE);
             }
-            cur_line[fileInd]++;
-        }
-    }
-
-    while (true) {
-        linebuf1[0] = '\0';
-        linebuf2[0] = '\0';
-
-        fgets(linebuf1, sizeof(linebuf1), file[0]);
-        fgets(linebuf2, sizeof(linebuf2), file[1]);
-
-        if (!strlen(linebuf1) && !strlen(linebuf2)) {
-            fprintf(stderr, "files are identical");
-            return(EXIT_SUCCESS);
+#ifdef DEBUG
+            printf("%lld: %s%s", cur_line[0], ">", linebuf1);
+#endif
+            cur_line[0]++;
         }
 
-        if (start_line[0] <= cur_line[0]
-                && strcmp(linebuf1, linebuf2) != 0) {
-            break;
+        while (start_line[1] > cur_line[1]) {
+            if (!fgets(linebuf1, sizeof(linebuf1), file[1])) {
+                perror("skip");
+                exit(EXIT_FAILURE);
+            }
+#ifdef DEBUG
+            printf("%lld: %s%s", cur_line[1], "<", linebuf1);
+#endif
+            cur_line[1]++;
         }
 
-        if (prevContext[0] != NULL)
-            free(prevContext[0]);
+    // Search for difference
+        while (true) {
+            linebuf1[0] = '\0';
+            linebuf2[0] = '\0';
 
-        prevContext[0] = prevContext[1];
-        prevContext[1] = prevContext[2];
-        prevContext[2] = prevContext[3];
-        prevContext[3] = prevContext[4];
-        prevContext[4] = strdup(linebuf1);
+            stored_offsets[0]  = ftello(file[0]);
+            stored_offsets[1]  = ftello(file[1]);
+            stored_linenums[0] = cur_line[0];
+            stored_linenums[1] = cur_line[1];
 
-        cur_line[0]++;
-        cur_line[1]++;
-    }
+            fgets(linebuf1, sizeof(linebuf1), file[0]);
+            fgets(linebuf2, sizeof(linebuf2), file[1]);
 
-    printf("found difference in %lld:%lld:\n", cur_line[0], cur_line[1]);
+#ifdef DEBUG
+            printf("%lld,%lld: %s%s", cur_line[0], cur_line[1], ">", linebuf1);
+            printf("%lld,%lld: %s%s", cur_line[0], cur_line[1], "<", linebuf2);
+#endif
 
-    for (i=0; i<5; i++) {
-        if (prevContext[i]) {
-            printf("%lld,%lld:  %s", cur_line[0] - (5-i), cur_line[1] - (5-i), prevContext[i]);
-        }
-    }
-
-    postContext[0][0] = strdup(linebuf1);
-    postContext[1][0] = strdup(linebuf2);
-
-    for(i=1; i<5; i++) {
-        linebuf1[0] = '\0';
-        linebuf2[0] = '\0';
-        fgets(linebuf1, sizeof(linebuf1), file[0]);
-        fgets(linebuf2, sizeof(linebuf2), file[1]);
-
-        postContext[0][i] = strdup(linebuf1);
-        postContext[1][i] = strdup(linebuf2);
-    }
-
-    fclose(file[0]);
-    fclose(file[1]);
-
-    int n;
-
-    meet[0] = 5;
-    meet[1] = 5;
-
-    for (i=0; i<5; i++) {
-        for (n=0; i+n < 5; n++) {
-            if (strcmp(postContext[0][i], postContext[1][i+n]) == 0) {
-                meet[0] = i;
-                meet[1] = i+n;
-                goto out;
+            if (!strlen(linebuf1) && !strlen(linebuf2)) {
+                return(EXIT_SUCCESS);
             }
 
-            if (strcmp(postContext[0][i+n], postContext[1][i]) == 0) {
-                meet[0] = i;
-                meet[1] = i+n;
-                goto out;
+            if (start_line[0] <= cur_line[0]
+                    && strcmp(linebuf1, linebuf2) != 0) {
+                break;
+            }
+
+            if (prevContext[0] != NULL)
+                free(prevContext[0]);
+
+            prevContext[0] = prevContext[1];
+            prevContext[1] = prevContext[2];
+            prevContext[2] = prevContext[3];
+            prevContext[3] = prevContext[4];
+            prevContext[4] = strdup(linebuf1);
+
+            cur_line[0]++;
+            cur_line[1]++;
+        }
+
+        if (!diffOnly)
+            printf("found difference in %lld:%lld:\n", cur_line[0], cur_line[1]);
+
+    // display context before difference
+        if (!diffOnly) {
+            for (i=0; i<5; i++) {
+                if (prevContext[i]) {
+                    printf("%lld,%lld:  %s", cur_line[0] - (5-i), cur_line[1] - (5-i), prevContext[i]);
+                }
             }
         }
-    }
 
-out:
+        postContext[0][0] = strdup(linebuf1);
+        postContext[1][0] = strdup(linebuf2);
 
-    for (i=0; i < 2; i++) {
-        for (n=0; n < meet[i] && n < 5; n++) {
-            printf("%lld,%lld: %s%s", cur_line[0]+n, cur_line[1]+n, i ? "+":"-", postContext[i][n]);
-        }
-    }
+    // load context after difference
+        for(i=1; i<5; i++) {
+            linebuf1[0] = '\0';
+            linebuf2[0] = '\0';
+            fgets(linebuf1, sizeof(linebuf1), file[0]);
+            fgets(linebuf2, sizeof(linebuf2), file[1]);
 
-    if (meet[0] < 5) {
-        printf("%lld,%lld:  %s", cur_line[0] + meet[0], cur_line[1] + meet[1], postContext[0][meet[0]]);
-
-        if (isCycled) {
-            start_line[0] = cur_line[0] + meet[0];
-            start_line[1] = cur_line[1] + meet[1];
-            continue;
+            postContext[0][i] = strdup(linebuf1);
+            postContext[1][i] = strdup(linebuf2);
         }
 
-        printf("\n"
-               "./showdiff -s%lld,%lld %s %s\n", cur_line[0] + meet[0], cur_line[1] + meet[1], filename[0], filename[1]);
-    }
+        fclose(file[0]);
+        fclose(file[1]);
 
-    break;
-} // loop
+     // find difference end
+        meet[0] = 5;
+        meet[1] = 5;
 
-    printf("\n debug: \n");
+        for (i=0; i<5; i++) {
+            for (n=0; i+n < 5; n++) {
+                if (strcmp(postContext[0][i], postContext[1][i+n]) == 0) {
+                    meet[0] = i;
+                    meet[1] = i+n;
+                    goto out;
+                }
 
-    printf("meet = %lld,%lld\n", cur_line[0] + meet[0], cur_line[1] + meet[1]);
+                if (strcmp(postContext[0][i+n], postContext[1][i]) == 0) {
+                    meet[0] = i;
+                    meet[1] = i+n;
+                    goto out;
+                }
+            }
+        }
 
-    for (n=0; n<5; n++) {
-        printf("%lld,%lld: %s%s", cur_line[0]+n, cur_line[1]+n, "-", postContext[0][n]);
-    }
+    out:
 
-    printf("\n");
+    // display difference
+        for (i=0; i < 2; i++) {
+            for (n=0; n < meet[i] && n < 5; n++) {
+                printf("%lld,%lld: %s%s", cur_line[0]+n, cur_line[1]+n, i ? "+":"-", postContext[i][n]);
+            }
+        }
 
-    for (n=0; n<5; n++) {
-        printf("%lld,%lld: %s%s", cur_line[0]+n, cur_line[1]+n, "+", postContext[1][n]);
-    }
+    // display context after difference
+        if (meet[0] < 5) {
+            if (!diffOnly)
+                printf("%lld,%lld:  %s", cur_line[0] + meet[0], cur_line[1] + meet[1], postContext[0][meet[0]]);
+
+        // restart search just after difference
+            if (isCycled) {
+                start_line[0] = cur_line[0] + meet[0];
+                start_line[1] = cur_line[1] + meet[1];
+
+#ifdef DEBUG
+                printf("\n debug: \n");
+
+                printf("meet = %lld,%lld\n", cur_line[0] + meet[0], cur_line[1] + meet[1]);
+
+                for (n=0; n<5; n++) {
+                    printf("%lld@0: %s%s", cur_line[0]+n, "-", postContext[0][n]);
+                }
+
+                printf("\n");
+
+                for (n=0; n<5; n++) {
+                    printf("%lld@1: %s%s", cur_line[1]+n, "+", postContext[1][n]);
+                }
+#endif
+
+                continue;
+            }
+        }
+
+    // difference end is not found, stop
+        break;
+    } // search restart loop
+
+    printf("fail to find difference end");
 
     return 0;
 }
